c149ef7695da8070e9e03f6fc7bde4c49fc7a872
neo-jayfeather <156867325+neo-jayfeather@users.noreply.github.com>
2024-05-14 15:49:13 -0400

    dE calculations for msts
    updated mst15-20 because of the extra lines

--- dE between two sets.py
+++ dE between two sets.py
@@ -1,5 +1,7 @@
 import numpy as np
-MST = 1
+
+MST = 2
+
 def dE76(point1, point2):
     L1, a1, b1 = point1
     L2, a2, b2 = point2
@@ -9,7 +11,7 @@ def dE76(point1, point2):
     delta_e = np.sqrt(delta_L**2 + delta_a**2 + delta_b**2)
     return delta_e
 
-def openMST(filename, file2):
+def openMST(filename, file2, output_filename):
     dE_values = []
     tarNumbers = []
     with open(filename, 'r') as file:
@@ -19,15 +21,24 @@ def openMST(filename, file2):
             for x in range(len(lines) - 1):
                 items = lines[x + 1].rstrip().replace('\t', ' ').split(' ')
                 item2 = line2[x + 1].rstrip().replace('\t', ' ').split(' ')
-                #print(10)
                 if items:
-                    L, a, b = float(items[7]), float(items[8]), float(items[9])                
+                    L, a, b = float(items[7]), float(items[8]), float(items[9])
                     target = (float(item2[7]), float(item2[8]), float(item2[9]))
                     cur_dE = dE76((L, a, b), target)
-                    print(cur_dE)
-                    
-        
-        
+                    dE_values.append(cur_dE)
+                    tarNumbers.append(items[0])
+
+    # Save the results to a text file
+    with open(output_filename, 'w') as output_file:
+        for x in range(len(dE_values)):
+            output_file.write(f"Patch {tarNumbers[x]}: dE = {dE_values[x]:.4f}\n")
+    print(f"Results saved to {outputFile}")
 file = f'10msts/mst{MST}.txt'
 otherFile = f'10msts/mst{MST+10}.txt'
-openMST(file,otherFile)
+outputFile = f'MST {MST} comparison_output.txt'
+for e in range(10):
+    MST = e + 1
+    file = f'10msts/mst{MST}.txt'
+    otherFile = f'10msts/mst{MST+10}.txt'
+    outputFile = f'MST {MST} comparison_output.txt'
+    openMST(file,otherFile, outputFile)
--- renumber.py
+++ renumber.py
@@ -1,31 +1,46 @@
-def replace_first_item(filename, new_filename):
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-        
-        with open(new_filename, 'w') as new_file:
-            for line in lines:
-                items = line.strip().split(' ')
-                if items:
-                    try:
-                        first_item = int(items[0])
-                        new_first_item = first_item - 68
-                        items[0] = str(new_first_item)
-                        new_line = ' '.join(items) + '\n'
-                        new_file.write(new_line)
-                    except ValueError:
-                        # Skip lines where the first item is not a valid integer
-                        new_file.write(line)
-                else:
-                    # Skip empty lines
-                    new_file.write(line)
-        
-        return f"Successfully created a new file '{new_filename}' with updated content!"
-    except FileNotFoundError:
-        return "File not found. Please provide valid filenames."
+import numpy as np
 
-# Example usage:
-input_filename = 'remeasureMSTS.txt'
-output_filename = 'remeasureMSTS1.txt'
-result = replace_first_item(input_filename, output_filename)
-print(result)
+MST = 2
+
+def dE76(point1, point2):
+    L1, a1, b1 = point1
+    L2, a2, b2 = point2
+    delta_L = L2 - L1
+    delta_a = a2 - a1
+    delta_b = b2 - b1
+    delta_e = np.sqrt(delta_L**2 + delta_a**2 + delta_b**2)
+    return delta_e
+
+def openMST(filename, file2, output_filename):
+    dE_values = []
+    tarNumbers = []
+    with open(filename, 'r') as file:
+        with open(file2, 'r') as file2:
+            lines = file.readlines()
+            line2 = file2.readlines()
+            for x in range(len(lines) - 1):
+                items = lines[x + 1].rstrip().replace('\t', ' ').split(' ')
+                item2 = line2[x + 1].rstrip().replace('\t', ' ').split(' ')
+                if items:
+                    L, a, b = float(items[7]), float(items[8]), float(items[9])
+                    target = (float(item2[7]), float(item2[8]), float(item2[9]))
+                    cur_dE = dE76((L, a, b), target)
+                    dE_values.append(cur_dE)
+                    tarNumbers.append(items[0])
+
+    # Save the results to a text file, ignoring blank lines
+    with open(output_filename, 'w') as output_file:
+        for x in range(len(dE_values)):
+            if tarNumbers[x].strip():  # Ignore blank lines
+                output_file.write(f"Patch {tarNumbers[x]}: dE = {dE_values[x]:.4f}\n")
+    print(f"Results saved to {outputFile}")
+
+file = f'10msts/mst{MST}.txt'
+otherFile = f'10msts/mst{MST+10}.txt'
+outputFile = f'MST {MST} comparison_output.txt'
+for e in range(10):
+    MST = e + 1
+    file = f'10msts/mst{MST}.txt'
+    otherFile = f'10msts/mst{MST+10}.txt'
+    outputFile = f'MST {MST} comparison_output.txt'
+    openMST(file, otherFile, outputFile)
